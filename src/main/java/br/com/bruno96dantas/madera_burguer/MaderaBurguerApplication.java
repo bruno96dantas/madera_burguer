48e79814048ccbfe39ddbac7a2deab6a231585ff
Bruno Dantas <bruno96dantas@gmail.com>
2019-02-14 13:17:52 -0200

    Init commit

---
+++ src/main/java/br/com/bruno96dantas/madera_burguer/MaderaBurguerApplication.java
@@ -0,0 +1,14 @@
+package br.com.bruno96dantas.madera_burguer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaderaBurguerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaderaBurguerApplication.class, args);
+	}
+
+}
+
